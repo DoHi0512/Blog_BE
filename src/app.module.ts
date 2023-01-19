bafd44bd4331b259c6627f1c96269d703b7abc53
DoHi0512 <donghuni512@gmail.com>
2023-01-19 20:36:10 +0900

    Update::Global Pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { BlogController } from './blog/blog.controller';
-import { PostController } from './post/post.controller';
-import { CommentController } from './comment/comment.controller';
-import { UserController } from './user/user.controller';
+
 
 
 @Module({
   imports: [],
-  controllers: [BlogController, PostController, CommentController, UserController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
