1f53afa70b775c0ccbca086ed83a0dd89bfc070a
DoHi0512 <donghuni512@gmail.com>
2023-02-14 20:22:12 +0900

    Update:: default 값으로 현재 날짜 들어가게 변경

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,5 +1,11 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
+const now = new Date();
+const today =
+  now.getFullYear().toString() +
+  '-' +
+  (now.getMonth() + 1).toString() +
+  '-' +
+  now.getDate().toString();
 @Entity()
 export class PostEntity {
   @PrimaryGeneratedColumn()
@@ -11,15 +17,15 @@ export class PostEntity {
   @Column()
   title: string;
 
-  @Column()
-  preview: string;
+  @Column({ default: 0 })
+  view: number;
 
-  @Column()
-  view: string;
-
-  @Column()
+  @Column({ default: today })
   date: string;
 
   @Column('simple-array')
   tag: string[];
+
+  @Column()
+  image: string;
 }
