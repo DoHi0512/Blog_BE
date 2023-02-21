a60a45af2830d98f85339d34a8c2e92ffff8a475
DoHi0512 <donghuni512@gmail.com>
2023-02-21 20:18:55 +0900

    Create:: Delete and Modify

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Post, Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Post,
+  Controller,
+  Get,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { CreatePostDTO } from './dto/create-post.dto';
+import { PostEntity } from './post.entity';
 import { PostService } from './post.service';
 
 @Controller('post')
@@ -22,5 +31,17 @@ export default class PostController {
   getById(@Param('id') id: number) {
     console.log('id');
     return this.postService.getById(id);
+  }
+
+  @Put('/modify')
+  modify(@Body() modifyPostDTO: PostEntity) {
+    console.log('modify');
+    return this.postService.modify(modifyPostDTO);
+  }
+
+  @Delete('/delete/:id')
+  delete(@Param('id') postId: number) {
+    console.log('delete');
+    return this.postService.delete(postId);
   }
 }
